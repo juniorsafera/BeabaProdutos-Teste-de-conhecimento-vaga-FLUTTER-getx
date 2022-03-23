13e1e32d8e4ac10d0da27e885f8e05a082d92ab9
Junior Safera <juniorsafera.dev@gmail.com>
2022-03-23 11:54:56 -0300

    Update main.dart

--- gerencioador_funcionarios/lib/main.dart
+++ gerencioador_funcionarios/lib/main.dart
@@ -1,27 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:gerencioador_funcionarios/components/form_cadastro.dart';
-import 'package:gerencioador_funcionarios/components/form_funcionario.dart';
-import 'package:gerencioador_funcionarios/modules/home/home_binding.dart';
-import 'package:gerencioador_funcionarios/modules/home/home_view.dart';
-import 'package:gerencioador_funcionarios/modules/initial/initial_view.dart';
+ 
   
 import 'package:get/get.dart';
 
-import 'utils/rotas.dart';
+import 'app/routes/app_pages.dart';
+import 'app/routes/app_routes.dart';
+
+ 
 
 void main() {
   runApp(GetMaterialApp(
     initialRoute: Rotas.initial,
-    getPages: [
-      GetPage(
-        name: Rotas.initial,
-        page: () => const InitinalView(),
-        binding: HomeBinding(),
-      ),
-      GetPage(name: Rotas.home, page: () => const HomeView()),
-      GetPage(name: Rotas.cadastro, page: () => const FormCadastro()),
-      GetPage(name: Rotas.formulario, page: () => const FormFuncionario()),
-    ],
+    getPages: AppPages.routes,
   ));
 }
 
