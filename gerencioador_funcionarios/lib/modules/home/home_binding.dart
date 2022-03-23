11188d9e528794910cd0c31f6899d4f96ea0d80d
Junior Safera <juniorsafera.dev@gmail.com>
2022-03-22 22:47:38 -0300

    Create home_binding.dart

---
+++ gerencioador_funcionarios/lib/modules/home/home_binding.dart
@@ -0,0 +1,11 @@
+import 'package:gerencioador_funcionarios/modules/home/home_controller.dart';
+import 'package:get/get.dart';
+
+class HomeBinding implements Bindings {
+  @override
+  void dependencies() {
+    Get.lazyPut<HomeController>(
+      () => HomeController(),
+    );
+  }
+}
