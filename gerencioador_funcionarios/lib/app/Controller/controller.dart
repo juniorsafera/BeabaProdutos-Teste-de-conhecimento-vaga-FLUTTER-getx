93ea7f8a8f460ca96b3b7884bc0340130e243f01
Junior Safera <juniorsafera.dev@gmail.com>
2022-03-23 11:57:10 -0300

    Create controller.dart

---
+++ gerencioador_funcionarios/lib/app/Controller/controller.dart
@@ -0,0 +1,65 @@
+import 'dart:math';
+import 'package:gerencioador_funcionarios/app/data/dados.dart';
+import 'package:gerencioador_funcionarios/app/model/model_funcionario.dart';
+import 'package:get/get.dart';
+
+class Controller extends GetxController {
+  
+  static Dados dados = Dados();
+
+  final Map<String, ModelFuncionario> _funcionarios = {...dados.listaFuncionarios};
+
+  List<ModelFuncionario> get funcionariosCadastrados =>
+      [..._funcionarios.values];
+
+  int get count {
+    return _funcionarios.length;
+  }
+
+  void cadastrar(ModelFuncionario funcionario) {
+    // ADICIONAR UM NOVO FUNCIONARIO NO BANCO
+    final id = Random().nextDouble().toString();
+    _funcionarios.putIfAbsent(
+      id,
+      () => ModelFuncionario(
+        id: id,
+        nome: funcionario.nome,
+        cargo: funcionario.cargo,
+        setor: funcionario.setor,
+        dataNascimento: funcionario.dataNascimento,
+        dataContratacao: DateTime.now(),
+        demitido: false,
+      ),
+    );
+
+    update();
+  }
+
+  void remover(ModelFuncionario funcionario) {
+    // REMOVER FUNCIONARIO DO BANCO
+    if (funcionario != null && funcionario.id != null) {
+      _funcionarios.remove(funcionario.id);
+      update();
+    }
+  }
+
+  void alterar(ModelFuncionario funcionario) {
+    // ATERAR DADOS DE FUNCIONARIO
+    _funcionarios.update(
+      funcionario.id,
+      (_) => ModelFuncionario(
+        id: funcionario.id,
+        nome: funcionario.nome,
+        cargo: funcionario.cargo,
+        setor: funcionario.setor,
+        dataNascimento: funcionario.dataNascimento,
+        dataContratacao: funcionario.dataContratacao,
+        demitido: funcionario.demitido,
+        dataDemissao: funcionario.dataDemissao,
+      ),
+    );
+    update();
+  }
+
+   
+}
